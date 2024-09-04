eaa9aa1a1e2659eab1ebafa027246688a2e0e892
Ignacio Zamo <ignaciozamo1@gmail.com>
2024-09-03 22:08:36 -0300

    Se trabaja en la seccion de galeria con preact(en proceso)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), tailwind(), react()]
+  integrations: [icon(), tailwind(), preact()]
 });
